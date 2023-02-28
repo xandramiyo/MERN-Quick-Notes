6df674574d3057ea9d42b65ad48345d38610ab3d
xandramiyo <aj.sumiyoshi@gmail.com>
2023-02-28 15:21:46 -0800

    removes checkToken

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,4 +1,4 @@
-import { checkToken } from '../../utilities/users-service'
+
 
 export default function OrderHistoryPage() {
 
@@ -10,7 +10,7 @@ export default function OrderHistoryPage() {
     return (
         <>
             <h1>OrderHistoryPage</h1>
-            <button onClick={handleCheckToken}>Check When my Login Expires</button>
+            
         </>
     )
 }
